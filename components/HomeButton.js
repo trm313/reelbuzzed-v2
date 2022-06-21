e93620fe51c928396951018bb03515c0b97badd3
Taylor <trm313@gmail.com>
2022-06-20 21:18:52 -0400

    Header styling and responsiveness

--- components/HomeButton.js
+++ components/HomeButton.js
@@ -1,10 +1,15 @@
-import { Flex, Button, Icon } from "@chakra-ui/react";
+import { Flex, Button, Icon, IconButton } from "@chakra-ui/react";
 import { IoArrowBack } from "react-icons/io5";
 import Link from "next/link";
 
 const HomeButton = () => (
   <Link href='/'>
-    <Button leftIcon={<Icon as={IoArrowBack} />}>Home</Button>
+    <IconButton
+      icon={<Icon as={IoArrowBack} />}
+      size={"md"}
+      aria-label='Return to Home'
+      colorScheme='gray'
+    />
   </Link>
 );
 
--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,20 +1,28 @@
 import { Flex, Spacer, Text, Divider, Heading } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import HomeButton from "../HomeButton";
 
 const Header = () => {
   const router = useRouter();
 
-  console.log("Header", router);
   return (
-    <Flex px={8} direction='column'>
-      <Flex w='100%' maxW='4xl' mx='auto' p={4} justifyContent='space-between'>
+    <Flex px={[0, 8]} direction='column'>
+      <Flex
+        alignItems='center'
+        w='100%'
+        maxW='4xl'
+        mx='auto'
+        p={4}
+        justifyContent='space-between'
+      >
         {router.asPath === "/" ? <Spacer /> : <HomeButton />}
-        <Flex direction='column' flexGrow={1} alignItems='center'>
-          <Heading>ReelBuzzed</Heading>
-          <Text>No Frills Movie Drinking Games</Text>
-        </Flex>
-
+        <Link href='/'>
+          <Flex direction='column' flexGrow={1} alignItems='center'>
+            <Heading>ReelBuzzed</Heading>
+            <Text>No Frills Movie Drinking Games</Text>
+          </Flex>
+        </Link>
         <Spacer />
       </Flex>
       <Divider />
